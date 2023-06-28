09a9cc9e707d560f170fab7ffc1a9eb031106b54
Gertf <gertfou@gmail.com>
2023-06-28 17:51:17 +0700

    vector python script added and requirements

--- watch_next.py
+++ watch_next.py
@@ -1,16 +1,15 @@
 import spacy
-import numpy as py
 
-nlp = spacy.load("en")
+nlp = spacy.load('en_core_web_md')
 
-#Creating an empty dictionary
+# Creating an empty dictionary
 d = {}
 
-#Reading file
+
 myfile = open("movies.txt").read()
 myfile = myfile.split('\n')
 myfile.pop()
-#Saving contents of file in a dictionary
+# Saving contents of file in a dictionary
 for i in myfile:
   d[i.split(":")[0]] = i.split(":")[1]
 
@@ -18,23 +17,26 @@ for i in myfile:
 
 l1 = []
 for i in d.values():
-  sentence_to_compare = '''Will he save their world or destroy it? 
-  When the Hulk becomes too dangerous for the Earth, the 
-  Illuminati trick Hulk into a shuttle and launch him into space 
-  to a planet where the Hulk can live in peace. Unfortunately, 
-  Hulk land on the planet Sakaar where he is sold into slavery and trained as a gladiator'''
+  sentence_to_compare = '''Will he save their world or destroy it?
+ When the Hulk becomes too dangerous for the Earth, the Illuminati 
+ trick Hulk into a shuttle and launch him into space to a planet 
+ where the Hulk can live in peace. Unfortunately, Hulk land on the 
+ planet Sakaar where he is sold into slavery and trained as a gladiator'''
 
-  model_sentences = nlp(sentence_to_compare)
+model_sentences = nlp(sentence_to_compare)
 
-
-
-#Comparing each movie description with the given dexcription to find the closest simillarity
+# Comparing each movie description with the given description to find the closest similarity
 similarity = nlp(i).similarity(model_sentences)
-print(i + "-" + str(similarity))
+print(i + "-" + str(similarity)) # similarity sentence least similarity with -0.7483523503220888
 l1.append(similarity)
 
 movie_title = []
 for i in d.keys():
-  movie_title.append(i)
-#Printing the movie title
-print(movie_title[l1.index(max(l1))])
+    movie_title.append(i)
+
+print(movie_title[l1.index(max(l1))]) #Printing the movie title
+
+# movie A is given in the vector similarity
+
+
+
