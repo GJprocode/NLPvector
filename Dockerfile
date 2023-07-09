3375c6b38261eac11041fe104f58e78ae0fd2ecb
gertf <gertfou@gmail.com>
2023-07-09 11:51:15 +0700

    fixed nlp vector similarity code to predict the next movie to watch and added detailed descriptions

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,26 @@
+# base image is use for building the Docker image. The pypy image is 
+# a pre-built image that provides a Python interpreter with a 
+# focus on speed and memory efficiency.
 FROM pypy:latest
+# This instruction sets the working directory within the container to /app.
 WORKDIR /app
-COPY . /app
+# This line copies the requirements.txt file from the host 
+# (the system where the Docker build command is executed) 
+# to the /app directory inside the container. 
+COPY requirements.txt /app
+# This command runs the pip install command inside the container, 
+# installing the Python packages specified in the requirements.txt file. 
+# The -r flag tells pip to read the requirements from a file.
 RUN pip install -r requirements.txt
+# This line copies all the files and directories 
+# from the current directory on the host (where the Dockerfile is located) 
+# to the /app directory inside the container. 
+# It ensures that all the necessary application files are available 
+# within the container.
+COPY . /app
+# This instruction sets the default command 
+# to be executed when the container is run. 
+# It specifies the command as python watch_next.py,
+# it will run the Python script named watch_next.py
+# using the Python interpreter installed in the container.
 CMD ["python", "watch_next.py"]
--- watch_next.py
+++ watch_next.py
@@ -1,42 +1,90 @@
+''' If spacy below is installed in your virtual environment with all modules
+and still give the error import "spacy" could not be resolved,
+just press Ctrl + Shift + P (win)/ cmd + Shift + P(macOS)
+to open command pellet.
+type "Python: Select Interpreter" and select this option when it appears. 
+It will open a list of available Python interpreters.
+Look for your virtual environment in the list and select it. 
+It will typically be located in the .venv folder within your project directory
+This will solve your issue, just work on the python interpreter with the
+virtual environment with all modules installed'''
+
+
+# imports the spacy library, a Natural language processing python library
 import spacy
+
+'''Code below loads the English language model (en_core_web_md) provided by spaCy. 
+The model is responsible for processing text and generating word 
+vectors that represent the meaning of words and sentences'''
 
 nlp = spacy.load('en_core_web_md')
 
-# Creating an empty dictionary
-d = {}
+'''This line defines a function called find_similar_movie 
+that takes a description parameter. 
+this function will find the most similar movie based 
+on the provided description'''
+
+def find_similar_movie(description):
+# Code below opens the movies.txt file in read mode and 
+# reads its contents. It stores the content in the movie_data variable,
+# which will be a list of strings where each string represents
+# a line in the file.
+    with open("movies.txt", "r") as file:
+        movie_data = file.readlines()
+
+# Code below iterates over each line in movie_data. 
+# For each line, it splits the line into the movie title and description
+#  based on the colon (:) separator. 
+# The title is stored as the key in the 
+# movies dictionary, and the description is stored as the 
+# corresponding value. The strip() method is used to remove any leading 
+# or trailing whitespace from the title and description strings.
+    movies = {}
+    for line in movie_data:
+        title, desc = line.strip().split(":", 1)
+        movies[title.strip()] = desc.strip()
+
+# Below code calculate the similarity between the provided description 
+# and each movie description in the movies dictionary.
+#  It initializes an empty dictionary similarity_scores to store the 
+# similarity scores. The nlp object is used to process the text and 
+# generate word vectors for both the query description 
+# and each movie description. The similarity() method is then 
+# called on the query_vector and movie_vector to calculate the 
+# similarity score, which is stored in the similarity_scores 
+# dictionary with the movie title as the key.
+    similarity_scores = {}
+    query_vector = nlp(description)
+    for title, desc in movies.items():
+        movie_vector = nlp(desc)
+        similarity_scores[title] = query_vector.similarity(movie_vector)
+
+    # Find the movie with the highest similarity score
+    most_similar_movie = max(similarity_scores, key=similarity_scores.get)
+    return most_similar_movie
+
+# Given Example Movie description to compare to other movies descriptions and calculate
+# a recommended movie based on similarity 
+description = '''Will he save their world or destroy it? 
+When the Hulk becomes too dangerous for the Earth, the
+Illuminati trick Hulk into a shuttle and launch him into 
+space to a planet where the Hulk can live in peace.
+Unfortunately, Hulk land on the planet Sakaar where he is sold
+into slavery and trained as a gladiator.'''
+
+'''Below code finds the movie with the highest similarity score by using the max()
+function on the similarity_scores dictionary. 
+The key argument is set to similarity_scores.get to specify that
+the maximum should be determined based on the values of the dictionary. 
+The movie title with the highest similarity score is then returned as 
+the result of the function'''
+similar_movie = find_similar_movie(description)
+print("Recommended movie for you based on your viewing history is :", similar_movie)
 
 
-myfile = open("movies.txt").read()
-myfile = myfile.split('\n')
-myfile.pop()
-# Saving contents of file in a dictionary
-for i in myfile:
-  d[i.split(":")[0]] = i.split(":")[1]
-
-
-
-l1 = []
-for i in d.values():
-  sentence_to_compare = '''Will he save their world or destroy it?
- When the Hulk becomes too dangerous for the Earth, the Illuminati 
- trick Hulk into a shuttle and launch him into space to a planet 
- where the Hulk can live in peace. Unfortunately, Hulk land on the 
- planet Sakaar where he is sold into slavery and trained as a gladiator'''
-
-model_sentences = nlp(sentence_to_compare)
-
-# Comparing each movie description with the given description to find the closest similarity
-similarity = nlp(i).similarity(model_sentences)
-print(i + "-" + str(similarity)) # similarity sentence least similarity with -0.7483523503220888
-l1.append(similarity)
-
-movie_title = []
-for i in d.keys():
-    movie_title.append(i)
-
-print(movie_title[l1.index(max(l1))]) #Printing the movie title
-
-# movie A is given in the vector similarity
-
-
-
+# answer 
+# Recommended movie for you based on your viewing history is : Movie C
+'''A darkness swirls at the center of a world-renowned dance company, 
+one that will engulf the artistic director, an ambitious young dancer, 
+and a grieving psychotherapist. 
+Some will succumb to the nightmare. Others will finally wake up.'''
